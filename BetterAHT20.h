fc72099cd666513af64244a5187ec57e38148b1c
Pygramer78 <ayuskyap@gmail.com>
2025-06-14 15:55:13 +0200

    Added Everything Carefully

---
+++ BetterAHT20.h
@@ -0,0 +1,10 @@
+#ifndef AHT_20
+#define AHT_20
+#include <AHT20.h>
+#include <Arduino.h>
+extern AHT20 aht20;
+void AHT20_init(void);
+void AHT20_readTemperature(void);
+void AHT20_readHumidity(void);
+void AHT20_read(void);
+#endif
